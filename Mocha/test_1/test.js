e7309ddb09edc32533a0933a18d4d29ca800962d
kh.moh3en <kh.moh3en@live.com>
2023-11-11 23:16:34 +0100

    tast

---
+++ Mocha/test_1/test.js
@@ -0,0 +1,13 @@
+describe("pow", function(){
+    function makeTest(x){
+        let expected = x * x * x;
+        it(`${x} به توان سه میشود ${expected}`, function() {
+          assert.equal(pow(x, 3), expected);
+        });
+      }
+    
+      for (let x = 1; x <= 5; x++) {
+        makeTest(x);
+      }
+    
+    });
