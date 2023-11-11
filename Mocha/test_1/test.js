e56dfc7130a31adbba124ea4ac8a75f73bc11d39
kh.moh3en <kh.moh3en@live.com>
2023-11-11 23:24:02 +0100

    cheng

--- Mocha/test_1/test.js
+++ Mocha/test_1/test.js
@@ -1,13 +1,14 @@
-describe("pow", function(){
-    function makeTest(x){
-        let expected = x * x * x;
-        it(`${x} به توان سه میشود ${expected}`, function() {
-          assert.equal(pow(x, 3), expected);
-        });
-      }
-    
-      for (let x = 1; x <= 5; x++) {
-        makeTest(x);
-      }
-    
+describe("pow", function() {
+
+  function makeTest(x) {
+    let expected = x * x * x;
+    it(`${x} به توان سه میشود ${expected}`, function() {
+      assert.equal(pow(x, 3), expected);
     });
+  }
+
+  for (let x = 1; x <= 5; x++) {
+    makeTest(x);
+  }
+
+});
