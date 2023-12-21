144f3d281bc38107280bac68f806721ea6e94779
kh.moh3en <kh.moh3en@live.com>
2023-12-21 23:56:09 +0100

    exampel

---
+++ W3Schools/Arrays/Exampel.js
@@ -0,0 +1,8 @@
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]) //display the array item where the index is euqal to i
+    }
+}
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
